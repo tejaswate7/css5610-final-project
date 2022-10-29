0a3c53d2cda544facf1221fe202f3cc02a3f17b0
Tejas Wate <tejaswate@gmail.com>
2022-10-29 13:38:24 -0400

    Added logo and Nav routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import Home from './routes/Home/home.component'
 import { Routes, Route } from 'react-router-dom'
+import Navigation from "./routes/Navigation/navigation.component";
+
+const Shop = () => {
+    return <h1>This is Shop Page</h1>
+}
 const App = () => {
-    const categories = [
-        {
-            id: 1,
-            title:'Meals',
-            imageUrl: 'https://www.themealdb.com/images/media/meals/g046bb1663960946.jpg'
-
-        },
-        {
-            id: 2,
-            title:'Drinks',
-            imageUrl: 'https://www.thecocktaildb.com/images/ingredients/gin.png'
-
-        }
-    ]
   return (
       <Routes>
-          <Route path='/' element = {<Home/>}/>
+          <Route path='/' element = {<Navigation />}>
+              <Route index element={<Home />}/>
+              <Route path='shop' element={<Shop/>}/>
+          </Route>
       </Routes>
   );
 }
---
+++ src/routes/Navigation/navigation.component.jsx
@@ -0,0 +1,24 @@
+import { Outlet, Link } from 'react-router-dom'
+import { ReactComponent as RestaurantLogo } from '../../assets/logo.svg'
+import './navigation.styles.scss'
+import { Fragment } from 'react'
+
+const Navigation = () => {
+    return(
+        <Fragment>
+            <div className="navigation">
+                <Link className="logo-container" to="/">
+                    <RestaurantLogo className = "logo" width={50}/>
+                </Link>
+                <div className = "nav-links-container">
+                    <Link className="nav-link" to="/shop">
+                        BUY
+                    </Link>
+                </div>
+            </div>
+            <Outlet/>
+        </Fragment>
+    );
+}
+
+export default Navigation;
