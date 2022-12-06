f456105c6dd8c51b0084fe18703ba5c41189e33e
Tanuj Ahuja <tanuj.ahuja@postman.com>
2022-12-05 19:19:37 -0500

    setup reviews for cocktails

--- src/routes/cocktail/cocktail.component.jsx
+++ src/routes/cocktail/cocktail.component.jsx
@@ -1,16 +1,31 @@
 import {useParams} from "react-router-dom";
-import {findCocktailByIdThunk, findCocktailBySearchTermThunk} from "../../thunks/cocktail-thunk";
-import {useEffect} from "react";
+import {findCocktailByIdThunk} from "../../thunks/cocktail-thunk";import {useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
+import {collection, onSnapshot, query, where} from "firebase/firestore"
+import {createReviewsDoc} from "../../utils/firebase/firebase.utils";
+import { db } from "../../utils/firebase/firebase.utils";
+import CommentItem from "../comments/comment.component";
 
 const CocktailComponent = () => {
-    const {id} = useParams();
+    let [review, setReview] = useState('');
+    let [allReview, setAllReviews] = useState([])
+    const {id, rid} = useParams();
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(findCocktailByIdThunk(id));
     }, [id])
+    const q = query(collection(db, "comments"), where("rid", "==", rid));
+    useEffect(() =>
+            onSnapshot(q, (snapshot) =>
+                setAllReviews(snapshot.docs.map((doc) => ({...doc.data(), id: doc.id}))))
+        , [])
+
     const {currentCocktail, loading} = useSelector((state) => state.cocktail)
     const cocktail = currentCocktail[0]
+    const {currentUser} = useSelector((state) => state.user)
+    const reviewClickHandler = () => {
+        createReviewsDoc(review, currentUser.uid, currentUser.displayName, id, rid);
+    }
     return (
         <div>
             { loading ? (<h1>Loading...</h1>)
@@ -20,6 +35,34 @@ const CocktailComponent = () => {
                     <h3>Tags: {cocktail.strTags}</h3>
                     <p>Instructions: {cocktail.strInstructions}</p>
                 </div>)}
+
+            <div className="row">
+                <div className="col-5">
+           <textarea value={review} placeholder="Your thoughts?"
+                     className="form-control border-0"
+                     onChange={(event) => setReview(event.target.value)}>
+           </textarea>
+                        <div>
+                            <button className="rounded-pill btn btn-primary float-end mt-2 ps-3 pe-3 fw-bold"
+                                    onClick={reviewClickHandler}>
+                                Review
+                            </button>
+                        </div>
+                    </div>
+                    <div className="col-12"><hr/></div>
+                </div>
+
+            <ul className="list-group">
+                {
+                    allReview ?
+                        allReview.map(comment =>
+                            <CommentItem key={comment.id} comment={comment}></CommentItem>
+                        )
+                    : <li className="list-group-item">
+                            No Reviews
+                    </li>
+                }
+            </ul>
         </div>
     )
 }
