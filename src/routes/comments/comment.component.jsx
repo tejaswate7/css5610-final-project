2950b58440df3332c7779ba0347eb3c0d69e531e
Tanuj Ahuja <tanuj.ahuja@postman.com>
2022-12-07 20:35:53 -0500

    ui improvements for comment

--- src/routes/comments/comment.component.jsx
+++ src/routes/comments/comment.component.jsx
@@ -9,24 +9,31 @@ const CommentItem = (
     }) => {
     const {currentUser} = useSelector((state) => state.user)
     const deleteCommentHandler = async () => {
-        console.log("j");
-        console.log()
         const docRef = doc(db, "comments", comment.id)
         await deleteDoc(docRef)
     }
+    const date = new Date(comment.createdAt.seconds * 1000)
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    const year = date.getFullYear();
+    const month = months[date.getMonth()];
+    const day = date.getDate();
+    const hour = date.getHours();
+    const min = date.getMinutes();
+    const sec = date.getSeconds();
+    const time = day + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
+    console.log(time)
 
-    const userName = "a"
     return(
         <div>
             {
-    userName ?
+    comment.userName ?
         (<div className="row wd-border-color-grey pt-2">
         <div className="col-12">
             { currentUser.uid === comment.userId &&
             <i className="bi bi-x-lg float-end"
                onClick={() => deleteCommentHandler()}></i>}
             <p style={{"fontSize": "15px"}} dangerouslySetInnerHTML={{__html: comment.comment}}></p>
-            <p>by {userName}</p>
+            <p style={{"fontSize": "12px"}}><i>by <strong>{comment.userName} at {time}</strong></i></p>
         </div>
     </div>) : (<h2>None</h2>)}
         </div>
